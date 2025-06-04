6120b7521d2fe787aba0def07b7d09665f75b28b
devobeng <dev.obeng.bismark@gmail.com>
2025-06-04 10:10:23 +0000

    added bin list and card component

---
+++ src/components/shared/Spinner.tsx
@@ -0,0 +1,18 @@
+import loader from "../../assets/loader.gif";
+const Spinner = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vh",
+      }}
+    >
+      <img src={loader} alt="loading......." width={150} height={150} />
+    </div>
+  );
+};
+
+export default Spinner;
